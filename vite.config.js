28cddf9f0189167ba203c2c72ba3ac75a354bb7c
srija9059 <panchalamanoj124@gmail.com>
2025-07-24 22:38:51 +0530

    resloved conflicts

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   plugins: [react(),tailwindcss()],
-=======
-  plugins: [react(), tailwindcss()],
->>>>>>> 0e8c97b9eba45a3256fef4189f9320afac010008
 })
