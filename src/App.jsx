923e9b25355cde448ecd43fba3cc538fc44e480b
HarshaM39 <emp324439@amentcapital.com>
2025-07-25 17:34:21 +0530

    Merge:The discount Listing is merged with home page from the(SWEWEB-26)

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,23 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HouseList from './components/HouseList';
 import HouseDetail from './components/HouseDetail';
-
-const App = () => {
-import React from 'react';
 import Home from './Pages/home';
 
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
       <Router>
+           <Home />
         <Routes>
           <Route path="/" element={<HouseList />} />
           <Route path="/details/:id" element={<HouseDetail />} />
         </Routes>
       </Router>
-    <div className="font-sans">
-      <Home />
+    
     </div>
-  );
-};
-export default App;
-  );
-}
+  )
+  }
 
-export default App;
-
+  export default App
+  
