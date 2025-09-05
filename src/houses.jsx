63e1821b1466f9982748735945d13ea6b68020df
Pravalika <emp732029@amentcapital.com>
2025-09-05 19:04:41 +0530

    Added format of images and there description

--- src/houses.jsx
+++ src/houses.jsx
@@ -1,46 +1,100 @@
-import p1i1 from "../src/assets/property1_image1.jpg";
-import p1i2 from "../src/assets/property1_image2.jpg";
-import p1i3 from "../src/assets/property1_image3.jpg";
-import p1i4 from "../src/assets/property1_image4.jpg";
-import p1i5 from "../src/assets/property1_image5.jpg";
-import p1i6 from "../src/assets/property1_image6.jpg";
-import p1i7 from "../src/assets/property1_image7.jpg";
-import p1i8 from "../src/assets/property1_image8.jpg";
-import p1i9 from "../src/assets/property1_image9.jpg";
-import p1i10 from "../src/assets/property1_image10.png";
+import p1i1 from "../src/assets/property1_Image1.jpg";
+import p1i2 from "../src/assets/property1_Image2.jpg";
+import p1i3 from "../src/assets/property1_Image3.jpg";
+import p1i4 from "../src/assets/property1_Image4.jpg";
+import p1i5 from "../src/assets/property1_Image5.jpg";
+import p1i6 from "../src/assets/property1_Image6.jpg";
+import p1i7 from "../src/assets/property1_Image7.jpg";
+import p1i8 from "../src/assets/property1_Image8.jpg";
+import p1i9 from "../src/assets/property1_Image9.jpg";
+import p1i10 from "../src/assets/property1_Image10.jpg";
+import p1i11 from "../src/assets/property1_Image11.jpg";
+import p1i12 from "../src/assets/property1_Image12.jpg";
+import p1i13 from "../src/assets/property1_Image13.jpg";
+import p1i14 from "../src/assets/property1_Image14.jpg";
+import p1i15 from "../src/assets/property1_Image15.jpg";
+import p1i16 from "../src/assets/property1_Image16.jpg";
+import p1i17 from "../src/assets/property1_Image17.jpg";
+import p1i18 from "../src/assets/property1_Image18.jpg";
+import p1i19 from "../src/assets/property1_Image19.jpg";
 
-import p2i1 from "../src/assets/property2_image1.png";
-import p2i2 from "../src/assets/property2_image2.png";
-import p2i3 from "../src/assets/property2_image3.png";
-import p2i4 from "../src/assets/property2_image4.png";
-import p2i5 from "../src/assets/property2_image5.png";
-import p2i6 from "../src/assets/property2_image6.png";
-import p2i7 from "../src/assets/property2_image7.png";
-import p2i8 from "../src/assets/property2_image8.png";
-import p2i9 from "../src/assets/property2_image9.png";
-import p2i10 from "../src/assets/property2_image10.png";
+import p2i1 from "../src/assets/property2_Image1.jpg";
+import p2i2 from "../src/assets/property2_Image2.jpg";
+import p2i3 from "../src/assets/property2_Image3.jpg";
+import p2i4 from "../src/assets/property2_Image4.jpg";
+import p2i5 from "../src/assets/property2_Image5.jpg";
+import p2i6 from "../src/assets/property2_Image6.jpg";
+import p2i7 from "../src/assets/property2_Image7.jpg";
+import p2i8 from "../src/assets/property2_Image8.jpg";
+import p2i9 from "../src/assets/property2_Image9.jpg";
+import p2i10 from "../src/assets/property2_Image10.jpg";
+import p2i11 from "../src/assets/property2_Image11.jpg";
+import p2i12 from "../src/assets/property2_Image12.jpg";
+import p2i13 from "../src/assets/property2_Image13.jpg";
+import p2i14 from "../src/assets/property2_Image14.jpg";
 
-import p3i1 from "../src/assets/property3_image1.jpg";
-import p3i2 from "../src/assets/property3_image2.jpg";
-import p3i3 from "../src/assets/property3_image3.jpg";
-import p3i4 from "../src/assets/property3_image4.jpg";
-import p3i5 from "../src/assets/property3_image5.jpg";
-import p3i6 from "../src/assets/property3_image6.jpg";
-import p3i7 from "../src/assets/property3_image7.jpg";
-import p3i8 from "../src/assets/property3_image8.png";
-import p3i9 from "../src/assets/property3_image9.jpg";
-import p3i10 from "../src/assets/property3_image10.jpg";
+import p3i1 from "../src/assets/property3_Image1.jpg";
+import p3i2 from "../src/assets/property3_Image2.jpg";
+import p3i3 from "../src/assets/property3_Image3.jpg";
+import p3i4 from "../src/assets/property3_Image4.jpg";
+import p3i5 from "../src/assets/property3_Image5.jpg";
+import p3i6 from "../src/assets/property3_Image6.jpg";
+import p3i7 from "../src/assets/property3_Image7.jpg";
+import p3i8 from "../src/assets/property3_Image8.jpg";
+import p3i9 from "../src/assets/property3_Image9.jpg";
+import p3i10 from "../src/assets/property3_Image10.jpg";
+import p3i11 from "../src/assets/property3_Image11.jpg";
+import p3i12 from "../src/assets/property3_Image12.jpg";
+import p3i13 from "../src/assets/property3_Image13.jpg";
+import p3i14 from "../src/assets/property3_Image14.jpg";
+import p3i15 from "../src/assets/property3_Image15.jpg";
+import p3i16 from "../src/assets/property3_Image16.jpg";
+import p3i17 from "../src/assets/property3_Image17.jpg";
+import p3i18 from "../src/assets/property3_Image18.jpg";
+import p3i19 from "../src/assets/property3_Image19.jpg";
+import p3i20 from "../src/assets/property3_Image20.jpg";
+import p3i21 from "../src/assets/property3_Image21.jpg";
 
-import p4i1 from "../src/assets/property4_image1.jpg";
-import p4i2 from "../src/assets/property4_image2.jpg";
-import p4i3 from "../src/assets/property4_image3.jpg";
-import p4i4 from "../src/assets/property4_image4.jpg";
-import p4i5 from "../src/assets/property4_image5.jpg";
-import p4i6 from "../src/assets/property4_image6.jpg";
-import p4i7 from "../src/assets/property4_image7.jpg";
-import p4i8 from "../src/assets/property4_image8.jpg";
-import p4i9 from "../src/assets/property4_image9.jpg";
-import p4i10 from "../src/assets/property4_image10.jpg";
+import p4i1 from "../src/assets/property4_Image1.jpg";
+import p4i2 from "../src/assets/property4_Image2.jpg";
+import p4i3 from "../src/assets/property4_Image3.jpg";
+import p4i4 from "../src/assets/property4_Image4.jpg";
+import p4i5 from "../src/assets/property4_Image5.jpg";
+import p4i6 from "../src/assets/property4_Image6.jpg";
+import p4i7 from "../src/assets/property4_Image7.jpg";
+import p4i8 from "../src/assets/property4_Image8.jpg";
+import p4i9 from "../src/assets/property4_Image9.jpg";
+import p4i10 from "../src/assets/property4_Image10.jpg";
+import p4i11 from "../src/assets/property4_Image11.jpg";
+import p4i12 from "../src/assets/property4_Image12.jpg";
+import p4i13 from "../src/assets/property4_Image13.jpg";
+import p4i14 from "../src/assets/property4_Image14.jpg";
+import p4i15 from "../src/assets/property4_Image15.jpg";
+import p4i16 from "../src/assets/property4_Image16.jpg";
+import p4i17 from "../src/assets/property4_Image17.jpg";
+import p4i18 from "../src/assets/property4_Image18.jpg";
+import p4i19 from "../src/assets/property4_Image19.jpg";
+
+import p5i1 from "../src/assets/property5_Image1.jpg";
+import p5i2 from "../src/assets/property5_Image2.jpg";
+import p5i3 from "../src/assets/property5_Image3.jpg";
+import p5i4 from "../src/assets/property5_Image4.jpg";
+import p5i5 from "../src/assets/property5_Image5.jpg";
+import p5i6 from "../src/assets/property5_Image6.jpg";
+import p5i7 from "../src/assets/property5_Image7.jpg";
+import p5i8 from "../src/assets/property5_Image8.jpg";
+import p5i9 from "../src/assets/property5_Image9.jpg";
+import p5i10 from "../src/assets/property5_Image10.jpg";
+import p5i11 from "../src/assets/property5_Image11.jpg";
+import p5i12 from "../src/assets/property5_Image12.jpg";
+import p5i13 from "../src/assets/property5_Image13.jpg";
+import p5i14 from "../src/assets/property5_Image14.jpg";
+import p5i15 from "../src/assets/property5_Image15.jpg";
+import p5i16 from "../src/assets/property5_Image16.jpg";
+import p5i17 from "../src/assets/property5_Image17.jpg";
+import p5i18 from "../src/assets/property5_Image18.jpg";
+import p5i19 from "../src/assets/property5_Image19.jpg";
+import p5i20 from "../src/assets/property5_Image20.jpg";
 
 export const houses = [
   {
@@ -60,11 +114,23 @@ export const houses = [
       p1i8,
       p1i9,
       p1i10,
+      p1i11,
+      p1i12,
+      p1i13,
+      p1i14,
+      p1i15,
+      p1i16,
+      p1i17,
+      p1i18,
+      p1i19,
     ],
-    description: `This well-maintained 2BHK apartment located at  L.B Nagar ,Hyderabad ,500079. With a built-up area of 1600 Sq Ft, the home offers spacious 2 bedrooms, two modern bathrooms, 
-                a semi-furnished modular kitchen, and a scenic road-facing balcony. Positioned on the 10th floor of a 5-storey building, the apartment receives ample natural light and ventilation throughout the day.
-                The property is 5 years old and part of a secure, gated society that offers 24x7 security, lift access, power backup, and dedicated parking. It is ideal for small families or working professionals, offering 
-                excellent connectivity to tech parks, hospitals, schools, and metro stations. With a monthly rent of ₹15,000 and a refundable deposit of ₹1,50,000, this home strikes the perfect balance between comfort, convenience, and affordability.`,
+    description: `Step into this thoughtfully designed 2BHK home that blends comfort, functionality, and style. The apartment features two well-ventilated bedrooms with built-in wardrobes, two modern bathrooms, and a semi-furnished modular kitchen with ample storage and workspace. Adding to its appeal is a dedicated pooja/prayer space, offering a peaceful corner for spiritual practices right within your home.
+
+The interiors are bright and airy, thanks to large windows and open balconies that invite in natural light and fresh air throughout the day. Elegant flooring and a clean layout make the home feel both cozy and sophisticated, perfect for families and working professionals alike.
+
+This property is part of a secure gated community, providing facilities such as 24x7 security, lift access, power backup, and dedicated parking. Every detail is designed to ensure safety, comfort, and convenience for its residents.
+
+Whether you’re unwinding in the balcony, spending time with loved ones in the spacious living areas, or enjoying the peaceful community atmosphere, this home is an ideal place to create lasting memories. With a competitive monthly rent of ₹[insert rent] and a refundable deposit of ₹[insert deposit], this 2BHK offers a rare mix of elegance, practicality, and affordability.`,
     beds: 2,
     size: '1600 Sq Ft',
     vehicles: '2 2W+1 4W',
@@ -107,11 +173,16 @@ export const houses = [
       p2i8,
       p2i9,
       p2i10,
+      p2i11,
+      p2i12,
+      p2i13,
+      p2i14,
     ],
-    description: `This well-maintained 2BHK apartment located at  'S.R Nagar ,Hyderabad ,50089  With a built-up area of 1800 Sq Ft, the home offers spacious 2 bedrooms, two modern bathrooms, a semi-furnished modular kitchen, 
-                and a scenic road-facing balcony. Positioned on the 12th floor of a 5-storey building, the apartment receives ample natural light and ventilation throughout the day.
-                The property is 5 years old and part of a secure, gated society that offers 24x7 security, lift access, power backup, and dedicated parking. It is ideal for small families or working professionals, 
-                offering excellent connectivity to tech parks, hospitals, schools, and metro stations. With a monthly rent of ₹16,000 and a refundable deposit of ₹1,50,000, this home strikes the perfect balance between comfort, convenience, and affordability.`,
+    description: `Step into this thoughtfully designed 2BHK apartment that combines simplicity with modern elegance. The home features two well-planned bedrooms that allow for maximum comfort, two contemporary bathrooms fitted with sleek fixtures, and a spacious living and dining area that flows seamlessly, perfect for family gatherings or quiet evenings. The modular kitchen offers plenty of space for storage and efficient cooking, making it ideal for everyday use.
+
+Large windows and an open layout ensure the home receives abundant natural sunlight throughout the day, creating a cheerful and refreshing atmosphere. The clean finishes and well-ventilated rooms provide a sense of openness that is rare to find.
+
+The apartment is part of a secure residential complex with round-the-clock security, power backup, and dedicated parking facilities, ensuring peace of mind for its residents. Its modern design, practical floor plan, and well-maintained common areas make this home a wonderful choice for families as well as professionals seeking both comfort and convenience.`,
     beds: 2,
     size: '1800 Sq Ft',
     vehicles: '2 2W+1 4W',
@@ -153,12 +224,24 @@ export const houses = [
       p3i8,
       p3i9,
       p3i10,
+      p3i11,
+      p3i12,
+      p3i13,
+      p3i14,
+      p3i15,
+      p3i16,
+      p3i17,
+      p3i18,
+      p3i19,
+      p3i20,
+      p3i21,
     ],
-    description: `This well-maintained 2BHK apartment located BalaNagar ,Hyderabad ,500071'  With a built-up area of 1400 Sq Ft, the home offers spacious 2 bedrooms, two modern bathrooms, a semi-furnished modular kitchen, 
-          and a scenic road-facing balcony. Positioned on the 5th floor of a 5-storey building, the apartment receives ample natural light and ventilation throughout the day.The property is 5 years old and part of a secure, gated
-          society that offers 24x7 security, lift access, power backup, and dedicated parking. It is ideal for small families or working professionals, offering excellent connectivity to tech parks, hospitals, schools, and metro 
-          stations. With a monthly rent of ₹12,000 and a refundable deposit of ₹1,50,000, this home strikes the perfect balance between comfort, convenience, and affordability.`,
-    beds: 2,
+    description: `This 3BHK apartment is the definition of space, comfort, and modern style. It features three generously sized bedrooms with ample room for wardrobes, three bathrooms with stylish fittings, and a bright living room that invites natural light from all directions. The modular kitchen is designed for both efficiency and aesthetics, ensuring a smooth cooking experience for daily living.
+
+A highlight of this property is the balcony with sliding glass doors, which not only expands the living space but also offers refreshing outdoor views. The interiors are enhanced with premium tiling, wooden flooring in select bedrooms, and contemporary ceiling finishes, adding a touch of elegance to the overall ambiance.
+
+The property is part of a gated community that ensures 24x7 security, lift access, power backup, and reserved parking. Well-lit, ventilated, and designed with attention to detail, this apartment is perfect for those who want the benefits of modern living combined with the warmth of a homely environment.`,
+    beds: 3,
     size: '1400 Sq Ft',
     vehicles: '2 2W+1 4W',
     type: 'Flat',
@@ -202,13 +285,21 @@ export const houses = [
       p4i8,
       p4i9,
       p4i10,
+      p4i11,
+      p4i12,
+      p4i13,
+      p4i14,
+      p4i15,
+      p4i16,
+      p4i17,
+      p4i18,
+      p4i19,
     ],
-    description: `This well-maintained 2BHK apartment located at  'R.N.Reddy ,Hyderabad ,500079' With a built-up area of 1600 Sq Ft,
-          the home offers spacious 2 bedrooms, two modern bathrooms, a semi-furnished modular kitchen, and a scenic road-facing balcony.
-          Positioned on the 2nd floor of a 5-storey building, the apartment receives ample natural light and ventilation throughout the day.
-          The property is 5 years old and part of a secure, gated society that offers 24x7 security, lift access, power backup, and dedicated parking. 
-          It is ideal for small families or working professionals, offering excellent connectivity to tech parks, hospitals, schools, and metro stations. 
-          With a monthly rent of ₹10,000 and a refundable deposit of ₹1,50,000, this home strikes the perfect balance between comfort, convenience,and affordability.`,
+    description: `This fully furnished 2BHK apartment offers everything you need for a convenient and stylish lifestyle. With two comfortable bedrooms complete with wardrobes and beds, two well-maintained bathrooms, and a spacious living room, this home is designed for ease of living. The kitchen is fully functional, equipped with cabinets and essential fittings, making it ready for immediate use.
+
+A private balcony with city views provides a perfect space to unwind, enjoy a morning coffee, or simply relax after a long day. The thoughtful interiors, tiled flooring, and modern fittings enhance both comfort and style.
+
+As part of a well-kept residential community, residents enjoy 24-hour security, lift access, and designated parking spaces. This property is perfect for small families and working professionals who want a balance of comfort, safety, and modern conveniences without the hassle of additional furnishing.`,
     beds: 2,
     size: '1600 Sq Ft',
     vehicles: '2 2W+1 4W',
@@ -238,14 +329,34 @@ export const houses = [
     agentId: 1,
     title: 'Sun City My Homes 10th Floor',
     location: 'ShadNagar ,Hyderabad ,500079',
-    image: 'https://res.cloudinary.com/dvqrtjzvv/image/upload/v1756127204/img5_rcgpsf.jpg',
-    description: `This well-maintained 2BHK apartment located at  'Shad Nagar , Hyderabad ,500079' With a built-up area of 1600 Sq Ft, 
-                  the home offers spacious 2 bedrooms, two modern bathrooms, a semi-furnished modular kitchen, and a scenic road-facing balcony. 
-                  Positioned on the 10th  floor of a 5-storey building, the apartment receives ample natural light and ventilation throughout 
-                  the day.The property is 5 years old and part of a secure, gated society that offers 24x7 security, lift access, power backup,
-                  and dedicated parking. It is ideal for small families or working professionals, offering excellent connectivity to tech parks,
-                    hospitals, schools, and metro stations. With a monthly rent of ₹20,000 and a refundable deposit of ₹1,50,000, this home strikes
-                    the perfect balance between comfort, convenience, and affordability.`,
+    image: [
+      'https://res.cloudinary.com/dvqrtjzvv/image/upload/v1756127204/img5_rcgpsf.jpg',
+      p5i1,
+      p5i2,
+      p5i3,
+      p5i4,
+      p5i5,
+      p5i6,
+      p5i7,
+      p5i8,
+      p5i9,
+      p5i10,
+      p5i11,
+      p5i12,
+      p5i13,
+      p5i14,
+      p5i15,
+      p5i16,
+      p5i17,
+      p5i18,
+      p5i19,
+      p5i20,
+    ],
+    description: `Experience the charm of independent living with this spacious 2BHK home designed for comfort and flexibility. The property offers two bedrooms with plenty of natural light, two bathrooms, a large living room ideal for entertaining, a separate dining area, and a functional kitchen with storage shelves and cabinets. The open and practical layout makes it perfect for both families and professionals who appreciate extra space.
+
+The home also includes a balcony/terrace space, ideal for setting up a small garden, enjoying fresh air, or creating your own personal retreat. Unlike apartments, this property offers more privacy and a sense of individuality, making it especially appealing for tenants who value peace and quiet.
+
+Residents also benefit from continuous water supply, power backup, 24x7 security, and dedicated parking. Its thoughtful design ensures comfort, while its independence offers freedom and flexibility rarely found in apartment-style living.`,
     beds: 2,
     size: '1600 Sq Ft',
     vehicles: '2 2W+1 4W',
