8af645a806b677f390e57c71e23fe6a04c086525
HarshaM39 <emp324439@amentcapital.com>
2025-09-23 19:45:39 +0530

    fix : the reloading page is fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter,  Routes, Route } from 'react-router-dom';
 import Navbar from './components/NavBar';
 import LoginSignup from './Pages/LoginSignUpPage';
 import HouseDetail from './components/HouseDetail';
@@ -17,26 +17,24 @@ import BlogPage from './Pages/BlogPage';
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/Login" element={<LoginSignup />} />
-          <Route path="/properties" element={<PropertyPage />} />
-          <Route path="/properties/:id" element={<HouseDetail />} />
-          <Route path="/verified-listings" element={<VerifiedListings />} />
-          <Route path="/furniture-rewards" element={<FurnitureRewards />} />
-          <Route path="/up-to-day-rentals" element={<UpToDayRentals />} />
-          <Route path="/agent" element={<Agent />} />
-          <Route path="/agent/:id" element={<AgentDetails />} />
-          <Route path="/landlord" element={<Landlord />} />
-          <Route path="/myrentals" element={<ActiveRentals />} />
-          <Route path="/blogs" element={<BlogPage />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route exact path="/Login" element={<LoginSignup />} />
+        <Route exact path="/properties" element={<PropertyPage />} />
+        <Route path="/properties/:id" element={<HouseDetail />} />
+        <Route path="/verified-listings" element={<VerifiedListings />} />
+        <Route path="/furniture-rewards" element={<FurnitureRewards />} />
+        <Route path="/up-to-day-rentals" element={<UpToDayRentals />} />
+        <Route path="/agent" element={<Agent />} />
+        <Route path="/agent/:id" element={<AgentDetails />} />
+        <Route path="/landlord" element={<Landlord />} />
+        <Route path="/myrentals" element={<ActiveRentals />} />
+        <Route path="/blogs" element={<BlogPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
